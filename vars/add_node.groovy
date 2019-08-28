d1cfe885ee3323cb01c8a69aa0b9af0ec64eb921
Rémi Jouannet <remi.jouannet@outscale.com>
2019-08-28 16:18:05 +0200

    add node

--- vars/add_node.groovy
+++ vars/add_node.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String c, String d) {
     def b = new com.remijouannet.add_node()
     def a = b.get_current_instance_id()
     println(a)
+    println(c)
+    println(d)
     echo "test aa"
 }
